c2c33ed2f18b18bb27c4c66dc7d65f321b622b75
AzizHr <azizharkati20@gmail.com>
2023-02-14 19:07:42 +0100

    .

--- frontend/src/views/MyReservations.vue
+++ frontend/src/views/MyReservations.vue
@@ -95,6 +95,7 @@
             class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
           >
             <tr>
+              <th scope="col" class="px-6 py-3">ID</th>
               <th scope="col" class="px-6 py-3">RESERVED AT</th>
               <th scope="col" class="px-6 py-3">HALL NAME</th>
               <th scope="col" class="px-6 py-3">SEAT NUMBER</th>
@@ -108,6 +109,7 @@
               :key="my_res.id"
               class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
             >
+              <td class="px-6 py-4">{{ my_res.id }}</td>
               <th
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -178,18 +180,18 @@ export default {
           // Swal.fire({
           //   icon: "success",
           //   title: "Done!",
-          //   text: `${res.data.Success}`,
+          //   text: `${res.data}`,
           // });
           console.log(res.data);
         })
-        .catch((err) => {
-          // Swal.fire({
-          //   icon: "success",
-          //   title: "Done!",
-          //   text: `${err.data.Error}`,
-          // });
-          console.log(err.data);
-        });
+        // .catch((err) => {
+        //   Swal.fire({
+        //     icon: "success",
+        //     title: "Done!",
+        //     text: `${err.data.Error}`,
+        //   });
+        //   console.log(err.data);
+        // });
     },
   },
 };
